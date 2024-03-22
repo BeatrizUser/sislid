f40d762697e8fd1a0b9c94a19ac53f66b3f552c4
BeatrizUser <beatriz.machadosilva@gmail.com>
2024-03-21 21:31:41 -0300

    update no site

--- blogZito/urls.py
+++ blogZito/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('sobre/', views.sobre, name='sobre'),
     path('propostas/', views.propostas, name='propostas'),
-    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('', views.blog, name='blog'),
+    path('post<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- blogZito/views.py
+++ blogZito/views.py
@@ -41,6 +41,27 @@ def propostas(request):
 
     return render(request, 'propostas.html', {'form': form, 'posts': posts})
 
+def blog(request):
+    # Verifique se o método de requisição é POST
+    if request.method == 'POST':
+        form = PerguntaForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PerguntaForm()
+
+    posts = Post.objects.order_by('-published_date')
+    
+    return render(request, 'blog.html', {'form': form, 'posts': posts})
+
 def post_detail(request, post_id):
+     # Verifique se o método de requisição é POST
+    if request.method == 'POST':
+        form = PerguntaForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PerguntaForm()
+
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'post_detail.html', {'post': post})
+    return render(request, 'post_detail.html', {'form': form,'post': post})
